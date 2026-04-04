30694c503b4d86fc359244320117f092871b93af
Fragondruit <joshnigam99@gmail.com>
2026-04-04 16:11:32 -0500

    Remove gemini integration

--- CatClient.ts
+++ CatClient.ts
@@ -1,16 +1,5 @@
-import { GenerateImagesResponse, GoogleGenAI } from "@google/genai";
-
 class CatClient {
   private static readonly BASE_URL: string = "https://meowfacts.herokuapp.com";
-  private static readonly GOOGLE_MODEL_NAME: string = "gemini-3.1-flash-image-preview";
-
-  private static s_geminiClient: GoogleGenAI;
-
-  static {
-    this.s_geminiClient = new GoogleGenAI({
-      apiKey: require("./.env.json").geminiApiKey,
-    });
-  }
 
   private static queryGet(numFetch?: number): Promise<Response> {
     return fetch(`${CatClient.BASE_URL}/?count=${numFetch ?? 1}`, {
@@ -30,21 +19,6 @@ class CatClient {
     return this.queryGet(pageSize)
         .then((response) => response.json())
         .then((data) => data?.data ?? []);
-  }
-
-  static getFactWithImage() {
-    this.queryGet()
-        .then((response) => response.json())
-        .then((data) => this.getImage(data.data[0]))
-        .catch((error) => console.error(error));
-  }
-
-  static getImage(prompt: string): Promise<GenerateImagesResponse> {
-    this.s_geminiClient.models.list().then(console.log);
-    return this.s_geminiClient.models.generateImages({
-      model: this.GOOGLE_MODEL_NAME,
-      prompt: prompt,
-    });
   }
 }
 
--- main.js
+++ main.js
@@ -1,18 +1,7 @@
 import CatClient from "./CatClient.ts";
 
 async function main() {
-    // console.log(await CatClient.getBulkFacts(4));
-    await CatClient.getImage('Generate a cat.');
+    console.log(await CatClient.getFact());
 }
 
 main();
-
-// fetch(`https://meowfacts.herokuapp.com/?count=1`, {
-//     method: "GET",
-//     headers: {
-//         "Content-Type": "application/json",
-//         Accept: "application/json",
-//     },
-// }).then((response) => response.text())
-//     .then((data) => console.log(data));
-
